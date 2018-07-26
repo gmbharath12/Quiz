0a8054d61df869675558cc02325622b1fef6f1d5
Bharath G M <gmbharath12@gmail.com>
2018-07-26 00:24:49 -0700

    Add assets, controller logic to have the basic flow

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7 + 7",
+        "What is the capital of California?",
+        "What is Cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Sacromento",
+        "Grapes"
+    ]
+    
+    var currentIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentIndex += 1;
+        if currentIndex == questions.count {
+            currentIndex = 0
+        }
+        questionLabel.text = questions[currentIndex]
+        answerLabel.text = "???"
     }
-
-
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = answers[currentIndex]
+    }
 }
 
