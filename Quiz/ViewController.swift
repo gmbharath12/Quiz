2f2eb87b54267acd3d029fef15ea1853b88b3bdf
Apple <bharath@bharaths-macbook-pro.local>
2018-07-25 19:32:11 -0700

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Bharath Giriyapura Marulasiddappa on 7/25/18.
+//  Copyright © 2018 Bharath G M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
